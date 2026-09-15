0fc02d000d1d7716344d883939f49345c70ec9f2
- <mkmayank1999@gmail.com>
2026-09-15 11:32:53 +0530

    first commit

---
+++ mvc_restfulProject/src/main/java/com/jsp/mvc_restfulProject/dto/EmployeeDTO.java
@@ -0,0 +1,76 @@
+package com.jsp.mvc_restfulProject.dto;
+
+import java.time.LocalDate;
+
+public class EmployeeDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dataOfJoining;
+    private Boolean isActive;
+
+    // by default constructor
+public EmployeeDTO(){
+
+}
+
+//generate constructor
+    public EmployeeDTO(Long id, String name, String email, Integer age, LocalDate dataOfJoining, Boolean isActive) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.age = age;
+        this.dataOfJoining = dataOfJoining;
+        this.isActive = isActive;
+    }
+
+    //getter and setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getDataOfJoining() {
+        return dataOfJoining;
+    }
+
+    public void setDataOfJoining(LocalDate dataOfJoining) {
+        this.dataOfJoining = dataOfJoining;
+    }
+
+    public Boolean getActive() {
+        return isActive;
+    }
+
+    public void setActive(Boolean active) {
+        isActive = active;
+    }
+}
